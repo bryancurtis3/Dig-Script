d11132db303a47ddc5c8d14eb322df47caf63587
bryancurtis3 <curtispro97@gmail.com>
2016-05-25 14:12:17 -0400

    Add Base Script
    Not much to look at

---
+++ my_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# I don't know what I'm doing :)
+
+dig -f /Users/bryancurtis/Desktop/names.txt MX > /Users/bryancurtis/Desktop/mailexchange.txt
+
+dig -f /Users/bryancurtis/Desktop/names.txt A > /Users/bryancurtis/Desktop/answers.txt
